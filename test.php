00ddf96a272040f17e1b0df84dc980b4c0c60a21
Dmitry <40324321+inilim@users.noreply.github.com>
2025-12-27 21:00:18 +0300

    Update test.php

--- test.php
+++ test.php
@@ -3,13 +3,34 @@
 // файл для проверок процедур
 
 use Inilim\Tool\VD;
+use Inilim\Tool\Exp;
 use App\Repository\RecordRepository;
 
 require_once __DIR__ . '/boot.php';
 
 
-$rep = \DI(RecordRepository::class);
+$_SERVER;
 
-$res = $rep->getForMainPage();
+$h = Exp::normalizeHeaders();
+$request = [
+    'path'    => '',
+    'details' => [
+        'id'      => '...', // important
+        'headers' => [], // important
+        'query'   => [], // important
+        'method'  => 'GET', // important
+        'host'  => '', // important
+        'uri' => '', // ?
+        'target' => '', // ?
+        'protocol' => '',
+        'cookies' => [], // ?
+        'files' => [], // ?
+    ],
+    'body'    => '',
+];
+
+// path string
+// details array
+// body 
 
 VD::de($res);
