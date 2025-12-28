1745cf2970e08f478505ea400407248f1f712dd7
Dmitry <40324321+inilim@users.noreply.github.com>
2025-12-28 12:09:23 +0300

    Update phpstan.php

--- tools/phpstan.php
+++ tools/phpstan.php
@@ -12,29 +12,46 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $phpstan = __DIR__ . '/../vendor/bin/phpstan';
 $phpstan = Path::realPath($phpstan);
 $phpstan = Path::normalize($phpstan);
-$process = new Process(['php84', $phpstan, 'analyze']);
+$process = new Process(['php84', $phpstan, 'analyze', '--error-format=json']);
 $process->run();
 $output = $process->getOutput();
-$output = Str::unixNewLines($output, PHP_EOL);
-
+$output = Str::trim($output);
+$output = \json_decode($output, true);
+// VD::de($output);
 $errorSkip = 0;
-$output = \explode(PHP_EOL, $output);
-foreach ($output as $idx => $line) {
-    $line = Str::trim($line);
-    if (!Str::isMatch('/^\d+\s/', $line)) {
-        // VD::de($line);
+foreach ($output['files'] as $ptf => $errors) {
+    $errors = $errors['messages'];
+    foreach ($errors as $idx => $err) {
+        if (\str_contains($err['message'], 'undefined static method Inilim\\Tool\\')) {
+            unset($errors[$idx]);
+            $errorSkip++;
+            continue;
+        }
+    }
+
+    if (!$errors) {
         continue;
     }
-    if (\str_contains($line, 'undefined static method Inilim\\Tool\\')) {
-        unset($output[$idx]);
-        // VD::de($line);
-        $errorSkip++;
-        continue;
+
+    echo \sprintf('%s -------------------------------------------------------- 
+  %s
+ -------------------------------------------------------- %s', PHP_EOL, $ptf, PHP_EOL);
+
+    foreach ($errors as $err) {
+
+        echo \sprintf('  Line: %s', $err['line']) . PHP_EOL;
+        echo \sprintf('  Message: %s', $err['message']) . PHP_EOL;
+        if (isset($err['tip'])) {
+            echo \sprintf('  Tip: "%s"', $err['tip']) . PHP_EOL;
+        }
+        echo '  ' . $err['identifier'] . PHP_EOL;
+        echo '  --------------------------------------------------------';
+        echo PHP_EOL;
     }
+
+    // VD::de();
 }
 
-if ($errorSkip > 0) {
-    // VD::de(Arr::last($output));
-}
-
-echo \implode(PHP_EOL, $output);
+$countErrors = $output['totals']['file_errors'] - $errorSkip;
+echo PHP_EOL;
+echo \sprintf(' [ERROR] Found %s errors ', $countErrors);
