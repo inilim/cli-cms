ea7de8739978575708c518adf32fedfa9ac00975
Dmitry <40324321+inilim@users.noreply.github.com>
2025-12-31 12:59:50 +0300

    Update FillFnForDbService.php

--- src/Service/FillFnForDbService.php
+++ src/Service/FillFnForDbService.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace App\Service;
 
-use App\Exception\AppException;
 use Inilim\Tool\VD;
 use Inilim\Tool\Time;
 use Inilim\IPDO\IPDOSQLite;
@@ -16,18 +15,12 @@ final class FillFnForDbService
 {
     function __invoke(IPDOSQLite $connect): void
     {
-        $connect->connect();
-        $pdo = $connect->getPDO();
-
-        if (!$pdo) {
-            throw new AppException();
-        }
         // INFO CRC_32 возвращаем как строку, а то sqlite плохо воспринимает большие числа
-        $pdo->sqliteCreateFunction('CRC_32', static function ($value): string {
+        $connect->createFunction('CRC_32', static function ($value): string {
             return (string)\crc32((string)$value);
         }, 1);
         $cls = Time::__asClosure('unixMs');
         /** @var \Closure $cls */
-        $pdo->sqliteCreateFunction('UNIX_MS', $cls, 0);
+        $connect->createFunction('UNIX_MS', $cls, 0);
     }
 }
